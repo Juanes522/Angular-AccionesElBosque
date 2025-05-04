863e58082d81362f63bc1346086f2836ecaabfbd
MJPU18 <125421024+MJPU18@users.noreply.github.com>
2025-05-03 22:15:34 -0500

    Initial connection to backend for user registration
    Connected to the bakend user registry

--- src/app/services/auth-service.service.ts
+++ src/app/services/auth-service.service.ts
@@ -8,15 +8,86 @@ import { Observable } from 'rxjs';
 })
 export class AuthServiceService {
 
-  private API_SERVER = "http://localhost:8080/user/create"
+  private API_SERVER = "http://localhost:8080/alpaca/accounts/create"
+  
 
   constructor(
     private http: HttpClient,
     private messageService: MessageService
   ) { }
 
-  registerUser(userData: any): Observable<any> {
+  registerUser(formData: any): Observable<any> {
+     const userData = {
+      "contact": {
+        "email_address": formData.email,
+        "phone_number": formData.phone,
+        "street_address": [formData.address],
+        "city": formData.city,
+        "postal_code": formData.postalCode,
+        "state": ""
+      },
+      "identity": {
+        "given_name": formData.firstName,
+        "family_name": formData.lastName,
+        "date_of_birth": formData.birthDate,
+        "tax_id_type": "COL_CC",
+        "tax_id": "1021392516",
+        "country_of_citizenship": "COL",
+        "country_of_birth": "COL",
+        "country_of_tax_residence": "COL",
+        "funding_source": ["employment_income"],
+        "annual_income_min": "10000",
+        "annual_income_max": "10000",
+        "total_net_worth_min": "10000",
+        "total_net_worth_max": "10000",
+        "liquid_net_worth_min": "10000",
+        "liquid_net_worth_max": "10000",
+        "liquidity_needs": "does_not_matter",
+        "investment_experience_with_stocks": "over_5_years",
+        "investment_experience_with_options": "over_5_years",
+        "risk_tolerance": "conservative",
+        "investment_objective": "market_speculation",
+        "investment_time_horizon": "more_than_10_years",
+        "marital_status": "MARRIED",
+        "number_of_dependents": 5
+      },
+      "disclosures": {
+        "is_control_person": false,
+        "is_affiliated_exchange_or_finra": false,
+        "is_affiliated_exchange_or_iiroc": false,
+        "is_politically_exposed": false,
+        "immediate_family_exposed": false
+      },
+      "agreements": [
+        {
+          "agreement": "customer_agreement",
+          "signed_at": "2024-08-27T10:39:34+01:00",
+          "ip_address": "185.11.11.11"
+        },
+        {
+          "agreement": "margin_agreement",
+          "signed_at": "2020-09-11T18:09:33Z",
+          "ip_address": "185.13.21.99"
+        }
+      ],
+      "documents": [
+        {
+          "document_type": "identity_verification",
+          "document_sub_type": "passport",
+          "content": "/9j/Cg==",
+          "mime_type": "image/jpeg"
+        }
+      ],
+      "trusted_contact": {
+        "given_name": "xyz",
+        "family_name": "wyz",
+        "email_address": ""
+      },
+      "additional_information": "",
+      "account_type": ""
+    };
+  
+    console.log('Datos completos a enviar:', JSON.stringify(userData, null, 2));
     return this.http.post(this.API_SERVER, userData);
   }
-
 }
